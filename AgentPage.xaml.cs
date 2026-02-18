6475839ee0724426645784908dacbd03a4cf3901
Tiffoxy <alyona-gk@mail.ru>
2026-02-18 11:06:05 +0500

    Добавьте файлы проекта.

---
+++ AgentPage.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+            var currentAgent = GostenovaGlaskiSaveEntities.GetContext().Agent.ToList();
+            AgentsListView.ItemsSource = currentAgent;
+            ComboType.SelectedIndex = 0;
+            ComboSort.SelectedIndex = 0;
+            UpdateService();
+        }
+        int CountRecords;
+        int CountPage;
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+        int CurrentPage = 0;
+        private  void ChangePage(int direction,int? SelectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountRecords%10>0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            Boolean Ifupdate = true;
+            int min;
+            if(SelectedPage.HasValue)
+            {
+                if(SelectedPage >=0 &&  SelectedPage <= CountPage)
+                {
+                    CurrentPage = (int)SelectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for(int i = CurrentPage*10;i<min;i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch(direction)
+                {
+                    case 1:
+                        if(CurrentPage>0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                    case 2:
+                        if(CurrentPage <CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                        
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+                AgentsListView.ItemsSource = CurrentPageList;
+                AgentsListView.Items.Refresh();
+            }
+        }
+        private void UpdateService()
+        {
+            
+            try
+            {
+                var currentAgents = GostenovaGlaskiSaveEntities.GetContext().Agent.ToList();              
+                if (ComboType.SelectedItem != null)
+                {
+                    string selectedType = (ComboType.SelectedItem as TextBlock).Text;
+
+                    if (selectedType != "Все типы")
+                    {
+                        currentAgents = currentAgents.Where(p => p.AgentTypeTitle == selectedType).ToList();
+                    }
+                }
+                if (!string.IsNullOrEmpty(TBSearch.Text))
+                {
+                    string searchText = TBSearch.Text.ToLower();
+                    string cleanedSearchPhone = searchText
+                        .Replace("+", "")
+                        .Replace("(", "")
+                        .Replace(")", "")
+                        .Replace("-", "")
+                        .Replace(" ", "")
+                        .Replace("8", "7");
+
+                    currentAgents = currentAgents.Where(p =>
+                        // Поиск по названию
+                        (p.Title != null && p.Title.ToLower().Contains(searchText)) ||
+
+                        // Поиск по email
+                        (p.Email != null && p.Email.ToLower().Contains(searchText)) ||
+
+                        // Поиск по телефону
+                        (p.Phone != null && p.Phone
+                        .Replace("+", "")
+                        .Replace("(", "")
+                        .Replace(")", "")
+                        .Replace("-", "")
+                        .Replace(" ", "")
+                        .Replace("8", "7").
+                        Contains(cleanedSearchPhone))
+                    ).ToList();
+                }
+                if (ComboSort.SelectedIndex == 0)
+                {
+
+                }
+                if (ComboSort.SelectedIndex == 2)
+                {
+                    currentAgents = currentAgents.OrderBy(p => p.Title).ToList();
+                }
+                if (ComboSort.SelectedIndex == 2)
+                {
+                    currentAgents = currentAgents.OrderByDescending(p => p.Title).ToList();
+                }
+                if (ComboSort.SelectedIndex == 3)
+                {
+                    currentAgents = currentAgents.OrderBy(p => p.Priority).ToList();
+                }
+                if (ComboSort.SelectedIndex == 4)
+                {
+                    currentAgents = currentAgents.OrderByDescending(p => p.Priority).ToList();
+                }
+                if (ComboSort.SelectedIndex == 5)
+                {
+                    currentAgents = currentAgents.OrderBy(p => p.Discount).ToList();
+                }
+                if (ComboSort.SelectedIndex == 6)
+                {
+                    currentAgents = currentAgents.OrderByDescending(p => p.Discount).ToList();
+                }
+
+
+                // Показываем результат
+                AgentsListView.ItemsSource = currentAgents;
+                AgentsListView.ItemsSource = currentAgents;
+                TableList = currentAgents;
+                ChangePage(0, 0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+            
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateService();
+        }
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateService();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateService();
+        }
+
+        private void LeftDirBtn_Clik(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ChangePage(1,null);
+        }
+
+        private void PageListBox_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void RightDirBtn_Clik(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ChangePage(2,null);
+        }
+    }
+}
